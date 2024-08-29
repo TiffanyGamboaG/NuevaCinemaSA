58ee0d70f1e81186bff60d3dff73caf80354c611
Tiffany Gamboa Gonzalez <tiffany.gamboa.gonzalez@est.una.ac.cr>
2024-08-28 22:38:10 -0600

    Add new methods

---
+++ Cinema SA/Schedule.cpp
@@ -0,0 +1,55 @@
+#include "Schedule.h"
+
+Schedule::Schedule()
+{
+	date = " ";
+	startTime = " ";
+	endTime = " ";
+}
+
+Schedule::Schedule(string newDate, string newStartTime, string newEndTime)
+{
+	date = newDate;
+	startTime = newStartTime;
+	endTime = newEndTime;
+}
+
+void Schedule::setDate(string newDate)
+{
+	date = newDate;
+}
+
+void Schedule::setStartTime(string newStartTime)
+{
+	startTime = newStartTime;
+}
+
+void Schedule::setEndTime(string newEndTime)
+{
+	endTime = newEndTime;
+}
+
+string Schedule::getDate()
+{
+	return string(date);
+}
+
+string Schedule::getStardTime()
+{
+	return string(startTime);
+}
+
+string Schedule::getEndTime()
+{
+	return string(endTime);
+}
+
+string Schedule::toString()
+{
+	ostringstream text;
+	text << "Fecha " << date << "\n"
+		<< "Hora inical" << startTime << "\n"
+		<< "Hora final" << endTime << "\n";
+
+	return text.str();
+}
